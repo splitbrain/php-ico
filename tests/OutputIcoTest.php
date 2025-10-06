ec2aad0616f9b29f0e9cfe684e3441fca6b36a3f
Andreas Gohr <gohr@cosmocode.de>
2025-10-06 12:14:27 +0200

    proper namespace

--- tests/OutputIcoTest.php
+++ tests/OutputIcoTest.php
@@ -1,104 +1,99 @@
 <?php
 declare (strict_types=1);
 
-namespace PHP_ICO\Tests;
+namespace phpico\Tests;
 
-use PHP_ICO;
+use phpico\PhpIco;
 use PHPUnit\Framework\TestCase;
-use PHPUnit_Framework_TestCase;
 
-/**
- * Class EasyDBTest
- * @package ParagonIE\EasyDB\Tests
- */
 class OutputIcoTest extends TestCase
 {
 
     public function goodAddImageSingleProvider()
     {
-        return array(
-            array(
+        return [
+            [
                 __DIR__ . DIRECTORY_SEPARATOR . 'test-ico-1.gif',
-                array(),
-            ),
-            array(
+                [],
+            ],
+            [
                 __DIR__ . DIRECTORY_SEPARATOR . 'test-ico-1.jpg',
-                array(),
-            ),
-            array(
+                [],
+            ],
+            [
                 __DIR__ . DIRECTORY_SEPARATOR . 'test-ico-1.png',
-                array(),
-            ),
-            array(
+                [],
+            ],
+            [
                 __DIR__ . DIRECTORY_SEPARATOR . 'test-ico-1.gif',
-                array(16,16),
-            ),
-            array(
+                [16,16],
+            ],
+            [
                 __DIR__ . DIRECTORY_SEPARATOR . 'test-ico-1.jpg',
-                array(16, 16),
-            ),
-            array(
+                [16, 16],
+            ],
+            [
                 __DIR__ . DIRECTORY_SEPARATOR . 'test-ico-1.png',
-                array(16, 16),
-            ),
-            array(
+                [16, 16],
+            ],
+            [
                 __DIR__ . DIRECTORY_SEPARATOR . 'test-ico-1.gif',
-                array(
-                    array(16,16),
-                ),
-            ),
-            array(
+                [
+                    [16,16],
+                ],
+            ],
+            [
                 __DIR__ . DIRECTORY_SEPARATOR . 'test-ico-1.jpg',
-                array(
-                    array(16,16),
-                ),
-            ),
-            array(
+                [
+                    [16,16],
+                ],
+            ],
+            [
                 __DIR__ . DIRECTORY_SEPARATOR . 'test-ico-1.png',
-                array(
-                    array(16,16),
-                ),
-            ),
-        );
+                [
+                    [16,16],
+                ],
+            ],
+        ];
     }
 
     public function badAddImageSingleProvider_invalidFile()
     {
-        return array(
-            array(
+        return [
+            [
                 null,
-                array(),
-            ),
-            array(
+                [],
+            ],
+            [
                 false,
-                array(),
-            ),
-            array(
+                [],
+            ],
+            [
                 true,
-                array(),
-            ),
-            array(
+                [],
+            ],
+            [
                 1,
-                array(),
-            ),
-            array(
+                [],
+            ],
+            [
                 '',
-                array(),
-            ),
-            array(
+                [],
+            ],
+            [
                 __DIR__ . DIRECTORY_SEPARATOR . 'test-ico-1.xcf',
-                array(),
-            ),
-        );
+                [],
+            ],
+        ];
     }
 
     public function badSaveIcoProvider_GetIcoDataReturnsFalse()
     {
-        return array(
-            array(
-                array(),
-            ),
-        );
+        return [
+            [
+                [],
+            ],
+        ];
     }
 
     /**
@@ -110,7 +105,7 @@ class OutputIcoTest extends TestCase
         $this->assertTrue(is_file($file . '.ico'));
         $this->assertTrue(is_readable($file . '.ico'));
 
-        $ico = new PHP_ICO($file, $sizes);
+        $ico = new PhpIco($file, $sizes);
         $outputToHere = tempnam(sys_get_temp_dir(), 'PHP_ICO_tests');
         $this->assertTrue($ico->save_ico($outputToHere));
         $this->assertSame(sha1_file($file . '.ico'), sha1_file($outputToHere));
@@ -121,7 +116,7 @@ class OutputIcoTest extends TestCase
     * @dataProvider badAddImageSingleProvider_invalidFile
     */
     public function testAddImageBadFiles($file, $sizes) {
-        $ico = new PHP_ICO();
+        $ico = new PhpIco();
         $this->assertFalse($ico->add_image($file, $sizes));
     }
 
@@ -130,7 +125,7 @@ class OutputIcoTest extends TestCase
     */
     public function testSaveIcoBadData($arrayOfFilesAndSizes)
     {
-        $ico = new PHP_ICO();
+        $ico = new PhpIco();
         foreach ($arrayOfFilesAndSizes as $file => $sizes)
         {
             $ico->add_image($file, $sizes);
